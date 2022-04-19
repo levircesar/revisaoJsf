f9e50723bc4dc0c084257dd1a45751b2ed1121c2
levircesar <levirlemos@gmail.com>
2022-04-19 20:24:14 -0300

    First commit

---
+++ src/main/java/bean/SegundoBean.java
@@ -0,0 +1,44 @@
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class SegundoBean {
+	private String nome;
+	private int idade;
+	private int resultado;
+	
+	public String salvar() {
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		setResultado(idade*3);
+		System.out.println("Resultado " + resultado);
+		return "cadastro";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(int resultado) {
+		this.resultado = resultado;
+	}
+}
